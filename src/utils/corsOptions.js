7fbf334a2477cf302e972fb713a80c344b422c1c
Volodymyr <Volodumurvel@gmail.com>
2024-07-20 07:55:49 +0200

    swagger and cors

--- src/utils/corsOptions.js
+++ src/utils/corsOptions.js
@@ -1,16 +1,16 @@
-// export const allowedOrigins = [
-//   'http://localhost:5173',
-//   'https://aqua-tracker-project-2.vercel.app',
-// ];
-// export const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   },
-//   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-//   allowedHeaders: ['Content-Type', 'Authorization'],
-//   credentials: true, // Allow credentials (cookies)
-// };
+export const allowedOrigins = [
+  'http://localhost:5173',
+  'https://aqua-tracker-project-2.vercel.app',
+];
+export const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true, // Allow credentials (cookies)
+};
